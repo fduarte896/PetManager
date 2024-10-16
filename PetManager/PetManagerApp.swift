85e7c198eaf284f34a6b57500c681ed42ebee2a1
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2024-10-16 08:53:50 +0200

    Initial Commit

---
+++ PetManager/PetManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetManagerApp.swift
+//  PetManager
+//
+//  Created by Laura Isabel Rojas Bustamante on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PetManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
